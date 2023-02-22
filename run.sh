28783c81376a7d622ba1e5db1c42a9073295bb32
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-02-22 17:33:12 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d /root/astar ]]; then
   BIN_NAME=$(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
-  echo -e "Downloading file: $bin_name"
+  echo -e "Downloading file: $BIN_NAME"
   cd /root
   wget ${BIN_NAME}
   tar -xvf astar-collator*.tar.gz
