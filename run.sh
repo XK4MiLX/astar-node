eb3dcf08624a42b307b78bfaa8009cc9b995e161
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-02-22 12:11:13 +0100

    initial

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ ! -d /root/astar ]]; then
+  bin_name=$(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
+  echo -e "Downloading file: $bin_name"
+  cd /root
+  wget $(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
+  tar -xvf astar-collator*.tar.gz
+  mv ./astar-collator /usr/local/bin
+  chmod +x /usr/local/bin/astar-collator
+  mkdir astar
+fi
+
+echo -e "Starting Astra Node: "
+NODE_NAME="AstraOnFlux-${RANDOM}"
+echo -e "Starting Astra Node: $NODE_NAME"
+astar-collator \
+  --pruning archive \
+  --rpc-cors all \
+  --name ${NODE_NAME} \
+  --chain astar \
+  --base-path /root/astar \
+  --execution Wasm \
+  --rpc-external \
+  --ws-external
