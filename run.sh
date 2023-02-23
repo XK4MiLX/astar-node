f7c06fa7732c33e6824bd9062d79104a47f577b9
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-02-23 12:07:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 if [[ ! -d /root/astar ]]; then
+  cd /root
+  echo "Creating config directory..."
+  mkdir astar
+fi
+
+if [[ ! -f /usr/local/bin/astar-collator ]]; then
+  cd /root
   BIN_NAME=$(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
   echo -e "Downloading file: $BIN_NAME"
-  cd /root
   wget ${BIN_NAME}
   tar -xvf astar-collator*.tar.gz
   mv ./astar-collator /usr/local/bin
   chmod +x /usr/local/bin/astar-collator
-  mkdir astar
+  rm -rf astar-collator*.tar.gz
 fi
 
-NODE_NAME="AstraOnFlux-${RANDOM}"
-echo -e "Starting Astra Node: $NODE_NAME"
+NODE_NAME="AstarOnFlux-${RANDOM}"
+echo -e "Starting Astar Node: $NODE_NAME"
 astar-collator \
   --pruning archive \
   --rpc-cors all \
