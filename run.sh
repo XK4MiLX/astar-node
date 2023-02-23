bf666aaab927496ab2c956c65bceb64486694f28
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-02-23 13:08:16 +0100

    removed download daemon file

--- run.sh
+++ run.sh
@@ -6,17 +6,6 @@ if [[ ! -d /root/astar ]]; then
   mkdir astar
 fi
 
-if [[ ! -f /usr/local/bin/astar-collator ]]; then
-  cd /root
-  BIN_NAME=$(curl -s https://api.github.com/repos/AstarNetwork/Astar/releases/latest | grep "tag_name" | awk '{print "https://github.com/AstarNetwork/Astar/releases/download/" substr($2, 2, length($2)-3) "/astar-collator-v" substr($2, 3, length($2)-4) "-ubuntu-x86_64.tar.gz"}')
-  echo -e "Downloading file: $BIN_NAME"
-  wget ${BIN_NAME}
-  tar -xvf astar-collator*.tar.gz
-  mv ./astar-collator /usr/local/bin
-  chmod +x /usr/local/bin/astar-collator
-  rm -rf astar-collator*.tar.gz
-fi
-
 NODE_NAME="AstarOnFlux-${RANDOM}"
 echo -e "Starting Astar Node: $NODE_NAME"
 astar-collator \
